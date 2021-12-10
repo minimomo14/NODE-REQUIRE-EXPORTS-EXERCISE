0afbeccfeef2fecfa37b0d199f858c6134c81d40
Momo Miller <minimomo14z@hotmail.com>
2021-12-10 17:03:26 -0500

    update data.js still needs to work on other part

---
+++ data.js
@@ -0,0 +1,13 @@
+let numArray = [1,1,2,3,5,8];
+let name = "Momo";
+let hometown = "Bangkok, Thailand";
+
+console.log(numArray);
+console.log(name);
+console.log(hometown);
+
+module.exports = {
+    numArray:numArray,
+    name:name,
+    hometown:hometown
+}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Hello World!!");
+
+const cowsay = require("cowsay");
+console.log(cowsay.think({text:"Moo-Moo"}));
+
+//const lodash = require("lodash");
+//console.log(lodash.say({text:"TGIF !!!"}));
